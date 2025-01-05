730262830b45d75c7fe0c5f7621c5246827208bd
Kiran Data Analyst <157777144+obitorin07@users.noreply.github.com>
2025-01-05 15:29:26 +0530

    day9

---
+++ JavaScript_Learning/first.js
@@ -0,0 +1,15 @@
+let name ='Kiran'
+let learning = 'Currently Learning About Web Development'
+console.log(name)
+console.log(learning)
+
+let a = 20;
+let b = 35;
+console.log('The addition of two number is :' ,a + b)
+
+// I'm very proud of my self i write this js basic code without googling or watch video and i never work with js before 😂i just did what is just tukka 
+
+// lets see how it go let me see the syntax from js it is similar to python 
+for (let i = 0; i < 3; i++){
+    console.log('Web Development')
+}
