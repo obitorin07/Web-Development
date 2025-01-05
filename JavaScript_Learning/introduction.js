1c8980b6a4aba92a0a28e5242826bd50460aabeb
Kiran Data Analyst <157777144+obitorin07@users.noreply.github.com>
2025-01-05 16:46:12 +0530

    Create introduction.js
    introduction code using js and this is day 9 learning about web dev

---
+++ JavaScript_Learning/introduction.js
@@ -0,0 +1,14 @@
+// introducing 
+let name = 'Kiran'
+let age = 22
+let interest = 'I Like Python and SQL'
+let current_goal = 'Learn About Web Development'
+
+console.log(`My Name is ${name}.\nmy age is ${age} \n${interest}  \nmy current goal is to ${current_goal} :)`)
+
+/* so i just googled it about f string in js like
+Python we use print(f'{age}') like this right
+so in js we can do same thing but differently
+
+using $ `
+*/
