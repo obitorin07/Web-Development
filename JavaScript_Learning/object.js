41bc7c668f3494f573b8c4789e08c2d6fed1b236
Kiran Data Analyst <157777144+obitorin07@users.noreply.github.com>
2025-01-09 21:25:04 +0530

    objects are like dict in python

---
+++ JavaScript_Learning/object.js
@@ -0,0 +1,15 @@
+//  objects are like dict in python
+
+myInterests = {'Langauge':[
+    "Python",'SQL' , 'SnowFlake', 'Js','Css','Data Analysis']
+} 
+
+console.log(myInterests.Langauge[0])
+
+myInterest2 = {'Langauge':[
+    "Python",'SQL' ,'SnowFlake', 'Js','Css','Data Analysis'],
+    'Tools_Technology':['Excel','Github','CMD', 'Power Bi']
+} 
+
+console.log(myInterest2.Tools_Technology);
+console.log(myInterest2.Tools_Technology[0]) ;
